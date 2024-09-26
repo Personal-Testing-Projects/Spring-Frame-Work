7f2600c467b84751193af7397bac20ad8f8afd96
pathumck <ckpathumck@gmail.com>
2024-09-26 20:50:22 +0530

    use scope annotation to create protype based objects

--- 03_Bean_Scopes/src/lk/ijse/spring/AppInitializer.java
+++ 03_Bean_Scopes/src/lk/ijse/spring/AppInitializer.java
@@ -1,6 +1,8 @@
 package lk.ijse.spring;
 
 import lk.ijse.spring.config.AppConfig;
+import lk.ijse.spring.pojo.PojoOne;
+import lk.ijse.spring.pojo.PojoTwo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInitializer {
@@ -9,5 +11,32 @@ public class AppInitializer {
         ctx.register(AppConfig.class);
         ctx.refresh();
         ctx.registerShutdownHook();
+
+        //to hear pojoone and pojotwo spring beans initializing without even calling them
+
+        //then add @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) annotation to PojoOne class
+        //now pojoone spring bean is not creating but pojotwo spring bean is creating
+
+        //check pojotwo's singleton behavior
+        PojoTwo pojoTwo1 = ctx.getBean(PojoTwo.class);
+        PojoTwo pojoTwo2 = ctx.getBean(PojoTwo.class);
+        //same reference is printing from below lines
+        System.out.println(pojoTwo1);
+        System.out.println(pojoTwo2);
+
+        //check pojoone's behavior with scope annotation
+        //now pojoone is instatntiated from above line
+        //it mean it needs to ask to make a object from pojoone now
+        ctx.getBean(PojoOne.class);
+
+        //check references
+        PojoOne pojoOne1 = ctx.getBean(PojoOne.class);
+        PojoOne pojoOne2 = ctx.getBean(PojoOne.class);
+        //different references are printing from above lines
+        //it means more object are creating using pojoone class
+        //prototype based objects are creating
+        System.out.println(pojoOne1);
+        System.out.println(pojoOne2);
+
     }
 }
--- 03_Bean_Scopes/src/lk/ijse/spring/pojo/PojoOne.java
+++ 03_Bean_Scopes/src/lk/ijse/spring/pojo/PojoOne.java
@@ -1,8 +1,11 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class PojoOne {
     public PojoOne() {
         System.out.println("PojoOne : Instantiated");
