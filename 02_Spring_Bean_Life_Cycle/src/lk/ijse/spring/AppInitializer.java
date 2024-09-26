0904c27dd3c0b5e742ad2638bf5fdc98b19417d2
pathumck <ckpathumck@gmail.com>
2024-09-26 19:58:22 +0530

    check spring bean life cycle by extending pojoone

--- 02_Spring_Bean_Life_Cycle/src/lk/ijse/spring/AppInitializer.java
+++ 02_Spring_Bean_Life_Cycle/src/lk/ijse/spring/AppInitializer.java
@@ -1,6 +1,7 @@
 package lk.ijse.spring;
 
 import lk.ijse.spring.config.AppConfig;
+import lk.ijse.spring.pojo.PojoOne;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInitializer {
@@ -8,6 +9,12 @@ public class AppInitializer {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
         ctx.register(AppConfig.class);
         ctx.refresh();
+        //spring beans will not destroy when ctx.registerShutdownHook() commented
         ctx.registerShutdownHook();
+
+        //to check in which state spring bean is ready to use
+        //below sout will execute after PojoOne : Initializing
+        PojoOne pojoOne = ctx.getBean(PojoOne.class);
+        System.out.println(pojoOne);
     }
 }
