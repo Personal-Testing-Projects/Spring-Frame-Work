7f97830b742f8dd21ffac9db91aa1e729bedf5fb
pathumck <ckpathumck@gmail.com>
2024-09-26 15:49:44 +0530

    set register shutdownhook

--- 01_Hello_Spring/src/lk/ijse/spring/AppInitializer.java
+++ 01_Hello_Spring/src/lk/ijse/spring/AppInitializer.java
@@ -3,6 +3,7 @@ package lk.ijse.spring;
 import lk.ijse.spring.config.AppConfig;
 import lk.ijse.spring.pojo.BasicDataSource;
 import lk.ijse.spring.pojo.Customer;
+import lk.ijse.spring.pojo.DbConnection;
 import lk.ijse.spring.pojo.PojoOne;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -15,7 +16,7 @@ public class AppInitializer {
         //catching the spring bean using two references to check it's singleton behavior
         Customer customerOne = ctx.getBean(Customer.class);
         Customer customerTwo = ctx.getBean(Customer.class);
-        //Same reference is printing twice by above lines
+        //Same reference is printing twice by below lines
         System.out.println(customerOne);
         System.out.println(customerTwo);
 
@@ -36,6 +37,30 @@ public class AppInitializer {
         PojoOne pojoOne = (PojoOne) ctx.getBean("ijse");
         System.out.println(pojoOne);
 
-        ctx.close();
+        //ctx.close();
+
+        ////////////////////*registerShutdownHook();*////////////////////
+
+        //can't access ctx after ctx.close()
+        //context destroy after ctx.close()
+        //error come when executing below line
+        //DbConnection dbConnection = ctx.getBean(DbConnection.class);
+
+        //so set ctx.close() to invoke almost when JVM shutdown from below call
+        /*Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ctx.close();
+            }
+        }));*/
+
+        //Now this line can execute
+        //DbConnection dbConnection = ctx.getBean(DbConnection.class);
+
+        //but above ctx.close() process is simplyfy by spring from below line
+        ctx.registerShutdownHook();
+
+        //Now the same process happens
+        DbConnection dbConnection = ctx.getBean(DbConnection.class);
     }
 }
