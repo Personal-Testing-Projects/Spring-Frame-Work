2493a8d2576223d036db9ae260653df3439eca05
pathumck <ckpathumck@gmail.com>
2024-09-26 12:00:46 +0530

    check singleton behavior of a spring bean using customer

--- 01_Hello_Spring/src/lk/ijse/spring/AppInitializer.java
+++ 01_Hello_Spring/src/lk/ijse/spring/AppInitializer.java
@@ -1,6 +1,7 @@
 package lk.ijse.spring;
 
 import lk.ijse.spring.config.AppConfig;
+import lk.ijse.spring.pojo.Customer;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInitializer {
@@ -8,6 +9,14 @@ public class AppInitializer {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
         ctx.register(AppConfig.class);
         ctx.refresh();
+
+        //catching the spring bean using two references to check it's singleton behavior
+        Customer customerOne = ctx.getBean(Customer.class);
+        Customer customerTwo = ctx.getBean(Customer.class);
+        //Same reference is printing twice by above lines
+        System.out.println(customerOne);
+        System.out.println(customerTwo);
+
         ctx.close();
     }
 }
