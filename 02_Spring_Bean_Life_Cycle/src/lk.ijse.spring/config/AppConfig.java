5090ce6d84c8e449f849b7a19f0c8799d10219ed
pathumck <ckpathumck@gmail.com>
2024-09-26 18:06:18 +0530

    create app initializer & app config

---
+++ 02_Spring_Bean_Life_Cycle/src/lk.ijse.spring/config/AppConfig.java
@@ -0,0 +1,10 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    public AppConfig() {
+        System.out.println("AppConfig : Intantiated");
+    }
+}
