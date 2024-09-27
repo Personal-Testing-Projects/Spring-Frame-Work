9c97830e1f2241e39454ee40fa556dea05930ec4
pathumck <ckpathumck@gmail.com>
2024-09-27 11:58:15 +0530

    check @Autowired annotation

--- 04_Spring_DI/src/lk/ijse/spring/AppInitializer.java
+++ 04_Spring_DI/src/lk/ijse/spring/AppInitializer.java
@@ -1,6 +1,7 @@
 package lk.ijse.spring;
 
 import lk.ijse.spring.config.AppConfig;
+import lk.ijse.spring.pojo.Boy;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInitializer {
@@ -9,5 +10,13 @@ public class AppInitializer {
         ctx.register(AppConfig.class);
         ctx.refresh();
         ctx.registerShutdownHook();
+
+        Boy boy = ctx.getBean(Boy.class);
+        boy.chatWithGirl();
+        //initializing two girl objects
+        //one by spring & otherone not from spring
+        //but this is wrong
+        //so we use @Autowired annotation to use that spring bean by dependency injection
+        //now only one girl object will initialized & that is a spring bean
     }
 }
--- 04_Spring_DI/src/lk/ijse/spring/pojo/Boy.java
+++ 04_Spring_DI/src/lk/ijse/spring/pojo/Boy.java
@@ -1,10 +1,48 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Boy {
+public class Boy implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
+    @Autowired
+    Girl girl;
+
     public Boy() {
         System.out.println("Boy : Instantiated");
+    }
+
+    public void chatWithGirl() {
+       /* Girl girl = new Girl();*/ // this is wrong. don't create objects
+        girl.chat();
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Boy : BeanFactoryAware");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Boy : BeanNameAware");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Boy : Destroy");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Boy : Initializing");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Boy : ApplicationContextAware");
     }
 }
