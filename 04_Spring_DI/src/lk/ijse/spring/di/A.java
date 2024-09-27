5fd32d1b40fc4332d4c2137c534c10fc9a319e60
pathumck <ckpathumck@gmail.com>
2024-09-27 15:11:43 +0530

    check spring setter method injection

--- 04_Spring_DI/src/lk/ijse/spring/di/A.java
+++ 04_Spring_DI/src/lk/ijse/spring/di/A.java
@@ -8,13 +8,18 @@ public class A {
     /*@Autowired*/
     SuperB superB;//Property Injection
 
-    @Autowired//contructor injection also works without this annotation
+    /*@Autowired*///contructor injection also works without this annotation
     public A(SuperB superB) {
-        this.superB = superB;//Constructor Injection
+        /*this.superB = superB;*///Constructor Injection
         System.out.println("A : Instantiated");
     }
 
     public void test() {
         superB.methodUseByA();
+    }
+
+    @Autowired
+    public void setSuperB(SuperB superB) { //Setter Method Injection
+        this.superB = superB;
     }
 }
