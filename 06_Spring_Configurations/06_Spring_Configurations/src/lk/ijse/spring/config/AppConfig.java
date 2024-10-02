d9a51dbc07308cbc31be7306d54f076359800c22
pathumck <ckpathumck@gmail.com>
2024-10-03 01:10:02 +0530

    create different configuration classes

--- 06_Spring_Configurations/06_Spring_Configurations/src/lk/ijse/spring/config/AppConfig.java
+++ 06_Spring_Configurations/06_Spring_Configurations/src/lk/ijse/spring/config/AppConfig.java
@@ -1,7 +1,6 @@
 package lk.ijse.spring.config;
 
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
