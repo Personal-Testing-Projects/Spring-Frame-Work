574274d023f44610071091358538aee857e43226
pathumck <ckpathumck@gmail.com>
2024-10-02 22:23:12 +0530

    create spring beans

---
+++ 05.Fullmode_&_Lightmode/src/lk/ijse/spring/pojo/PojoTwo.java
@@ -0,0 +1,37 @@
+package lk.ijse.spring.pojo;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class PojoTwo implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
+    public PojoTwo() {
+        System.out.println("\nPojoTwo : Instantiated");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("PojoTwo : " + name);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("PojoTwo : " + beanFactory);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("PojoTwo : " + applicationContext );
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("PojoTwo : Initializing");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("\nPojoTwo : Destroy");
+    }
+}
