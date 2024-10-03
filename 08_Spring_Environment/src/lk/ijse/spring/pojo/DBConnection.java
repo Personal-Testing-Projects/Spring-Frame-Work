508bab0ec4915e61ec7250651f8e004007cfd776
pathumck <ckpathumck@gmail.com>
2024-10-03 22:57:03 +0530

    check @PropertySource annotation

--- 08_Spring_Environment/src/lk/ijse/spring/pojo/DBConnection.java
+++ 08_Spring_Environment/src/lk/ijse/spring/pojo/DBConnection.java
@@ -1,10 +1,31 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DBConnection {
+public class DBConnection implements InitializingBean {
+    @Value("${os.name}")//property placeholder
+    private String myOSName;
+
+    @Value("${USERNAME}")
+    private String myUserName;
+
+    @Value("${db.user.name}")
+    private String dbUserName;
+    @Value("${db.user.password}")
+    private String dbPassword;
+
     public DBConnection() {
         System.out.println("DBConnection : Instantiated");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("OS Name : " + myOSName);
+        System.out.println("User Name : " + myUserName);
+        System.out.println("DB User Name : " + dbUserName);
+        System.out.println("DB Password : " + dbPassword);
     }
 }
