b14f9caa834a88a72020f5392dca51ef2dda9cc4
pathumck <ckpathumck@gmail.com>
2024-10-04 18:50:27 +0530

    create new module for spring mvc

---
+++ 09_Spring_Web_MVC/src/lk/ijse/spring/config/WebAppConfig.java
@@ -0,0 +1,26 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@Configuration
+@EnableWebMvc
+//@ComponentScan
+public class WebAppConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
